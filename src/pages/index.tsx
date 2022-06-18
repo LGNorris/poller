085cd80cb9ac2573158cca8455dcdd48cbe4545a
Lewis Norris <lewis.george.norris@gmail.com>
2022-06-18 19:24:46 +0100

    refactor backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { prisma } from "../db/client";
 import { trpc } from "../utils/trpc";
 
-const Home: NextPage = (props: any) => {
-    const { data, isLoading } = trpc.useQuery(["getAllQuestions"]);
+const Home: NextPage = () => {
+    const { data, isLoading } = trpc.useQuery(["questions.get-all"]);
     if (isLoading || !data) return <div> ...Loading</div>;
     return <div>{data[0].question}</div>;
 };
